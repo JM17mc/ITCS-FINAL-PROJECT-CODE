e2890a58e986381dfad24482782970b3c71dce51
JM17mc <jhnmchlmamac@gmail.com>
2024-12-13 19:19:55 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,84 @@
+#purpose of assignment operators
+#problem (code scenario)
+#let say we have variable
+
+x = 5
+
+print(x)
+
+x = x + 10
+
+print(x)
+
+x = x + 15
+
+print(x)
+
+x = x + 20
+
+print(x)
+
+x = x + 25
+
+print(x)
+
+x = x + 30
+
+print(x)
+
+x = x + 35
+
+print(x)
+
+x = x + 40
+
+print(x)
+
+x = x + 45
+
+print(x)
+
+x = x + 50
+
+print(x)
+
+x = x + 55
+
+print(x)
+
+x = x + 60
+
+print(x)
+
+x = x + 65
+
+print(x)
+
+x = x + 70
+
+print(x)
+
+x = x + 75
+
+print(x)
+
+x = x + 80
+
+print(x)
+
+x = x + 85
+
+print(x)
+
+x = x + 90
+
+print(x)
+
+x = x + 95
+
+print(x)
+
+x = x + 100
+
+print(x)
+
