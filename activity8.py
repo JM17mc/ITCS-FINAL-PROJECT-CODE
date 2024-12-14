03263b7fdde391befd52d1718938e735018a0ec7
JM17mc <jhnmchlmamac@gmail.com>
2024-12-14 16:40:01 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,13 @@
+password = input("Enter your password --> : ")
+
+if password.lower() == "secret" :
+	 print("Access Granted")
+	 print("Enjoy using the system, pogi!")
+	elif password.lower() == "passwordba2" :
+	 print("Access Granted")
+	 print("Ikaw na")
+
+	else:
+	print("Access Denied")
+
+print("System Exit")
