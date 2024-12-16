5645c4a7b0c5578befd9f37ad4b8affeccea9d1b
JM17mc <jhnmchlmamac@gmail.com>
2024-12-16 14:46:57 +0800

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,9 @@
+#washing machine
+
+for x in range(0, 11):
+    #print("*", end = " ") 
+    #another washing machine
+    print(x, end = "  ")
+    for y in range(x, 11):
+        print("*", end = "  ")
+    print()
