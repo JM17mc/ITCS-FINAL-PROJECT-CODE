3df0fb8c89104886774d9492f213935f47adebda
JM17mc <jhnmchlmamac@gmail.com>
2024-12-16 14:40:34 +0800

    Uactivity4.py

--- activity4.py
+++ activity4.py
@@ -1,5 +1,20 @@
 num1 = eval(input("Enter a number:  "))
 num2 = eval(input("Enter a number:  "))
-answer = num1 + num2
+number1 = eval(input("Enter a number: "))
+number2 = eval(input("Enter a number: "))
+sum = number1 + number2 
+diff = number1 - number2
+product = number1 * number2
+quot = number1 / number2
+exponent = number1 ** number2
+remainder = number1 % number2
+flordivision = number1 // number2
 
-print ("The sum of", num1, " + ", num2, "is" ,answer)
+
+print("The sum of " , number1 , " and" , number2 , "is" , sum)
+print("The difference of  " , number1 , " and" , number2 , "is" , diff)
+print("The product of " , number1 , " and" , number2 , "is" , product)
+print("The quotient of " , number1 , " and" , number2 , "is" , quot)
+print("The exponent of " , number1 , " and" , number2 , "is" , exponent)
+print("The remainder of " , number1 , " and" , number2 , "is" , remainder)
+print("The floor division of " , number1 , " and" , number2 , "is" , flordivision)
