7f3e4732bd648210f0f3aafeb5b7b496146eaae6
JM17mc <jhnmchlmamac@gmail.com>
2024-12-17 17:19:25 +0800

    Add files via upload

---
+++ activity19.py
@@ -0,0 +1,9 @@
+con = True
+while con == True:
+    name = input("Enter a name: ")
+
+    if name.lower() == "stop":
+        print("The loop has been terminated")
+
+        break
+        con = False 
