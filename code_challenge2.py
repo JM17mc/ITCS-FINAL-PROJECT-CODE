ec65f0021b6fc1f08fb978632d76a177b4dabdf8
JM17mc <jhnmchlmamac@gmail.com>
2024-12-18 18:37:00 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,3 @@
+name = input("type your name here -->  ")
+
+print ("\t\t\t\t\t\t\t\t\t\t  * \n\t\t\t\t\t\t\t\t\t\t* * *   \n\t\t\t\t\t\t\t\t\t      * * * * *     \n\t\t\t\t\t\t\t\t\t   * Hi, " + name + " *\n\t\t\t\t\t\t\t\t\t      * * * * *   \n\t\t\t\t\t\t\t\t\t\t* * *  \n \t\t\t\t\t\t\t\t\t\t  *")
